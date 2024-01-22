9c98abe2f79b0a04b67958075e2eb3716cb27741
Shweta-2007 <sinhashweta713@gmail.com>
2024-01-22 21:22:12 +0530

    paymentAPI

--- dist/controllers/payment.js
+++ dist/controllers/payment.js
@@ -3,21 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.applyDiscount = exports.newCoupon = void 0;
+exports.newCoupon = void 0;
 const error_1 = require("../middlewares/error");
 const coupon_1 = require("../models/coupon");
 const utility_class_1 = __importDefault(require("../utils/utility-class"));
 exports.newCoupon = (0, error_1.TryCatch)(async (req, res, next) => {
-    const { code, amount } = req.body;
-    if (!code || !amount)
-        return next(new utility_class_1.default("Please enter both fields", 400));
-    await coupon_1.Coupon.create({ code, amount });
-    return res.status(201).json({
-        success: true,
-        message: `Coupon Created ${code} Successfully`,
-    });
-});
-exports.applyDiscount = (0, error_1.TryCatch)(async (req, res, next) => {
     const { code, amount } = req.body;
     if (!code || !amount)
         return next(new utility_class_1.default("Please enter both fields", 400));
